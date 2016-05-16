5c566dc1105e9278acdf3f3318b541cc64c71d3c
abraxasrex <jbreasor@gmail.com>
2016-05-15 23:41:59 -0700

    setup midi device testing env

--- midi-test-env.js
+++ midi-test-env.js
@@ -1,6 +1,19 @@
 var easymidi = require('easymidi');
-var input = new easymidi.Input('*');
 
-input.on('*', function(msg){
+//get input names
+console.log('MIDI inputs:');
+console.log(easymidi.getInputs());
+
+//get output names
+console.log('MIDI outputs:');
+console.log(easymidi.getOutputs());
+
+//get note message specs
+input.on('noteOn', function(msg){
+  console.log(msg);
+});
+
+//get control message specs
+input.on('cc', function(msg){
   console.log(msg);
 })
